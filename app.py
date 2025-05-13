282dc11910bddc8371e5a53eb615793208b52d83
Jayant <jayantpr26@gmail.com>
2025-05-13 11:56:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,26 +10,32 @@ st.title("Faulty Steel Plate Prediction")
 st.write("Enter the steel plate characteristics to predict the type of fault.")
 
 # User Inputs
-X_Minimum = st.number_input("X_Minimum", value=0.0)
-X_Maximum = st.number_input("X_Maximum", value=0.0)
-Y_Minimum = st.number_input("Y_Minimum", value=0.0)
-Y_Maximum = st.number_input("Y_Maximum", value=0.0)
-Pixels_Areas = st.number_input("Pixels_Areas", value=1.0)
-X_Perimeter = st.number_input("X_Perimeter", value=1.0)
-Y_Perimeter = st.number_input("Y_Perimeter", value=1.0)
-Sum_of_Luminosity = st.number_input("Sum_of_Luminosity", value=1.0)
-Minimum_of_Luminosity = st.number_input("Minimum_of_Luminosity", value=0.0)
-Maximum_of_Luminosity = st.number_input("Maximum_of_Luminosity", value=0.0)
-Length_of_Conveyer = st.number_input("Length_of_Conveyer", value=0.0)
+X_Minimum = st.number_input("X_Minimum", value=42.0)
+X_Maximum = st.number_input("X_Maximum", value=50.0)
+Y_Minimum = st.number_input("Y_Minimum", value=270900.0)
+Y_Maximum = st.number_input("Y_Maximum", value=270944.0)
+Pixels_Areas = st.number_input("Pixels_Areas", value=267.0)
+X_Perimeter = st.number_input("X_Perimeter", value=17.0)
+Y_Perimeter = st.number_input("Y_Perimeter", value=44.0)
+Sum_of_Luminosity = st.number_input("Sum_of_Luminosity", value=24220.0)
+Minimum_of_Luminosity = st.number_input("Minimum_of_Luminosity", value=76.0)
+Maximum_of_Luminosity = st.number_input("Maximum_of_Luminosity", value=108.0)
+Length_of_Conveyer = st.number_input("Length_of_Conveyer", value=1687.0)
 TypeOfSteel = st.selectbox("TypeOfSteel", options=["A300", "A400"])
-Steel_Plate_Thickness = st.number_input("Steel_Plate_Thickness", value=1.0)
-Edges_Index = st.number_input("Edges_Index", value=0.0)
-Empty_Index = st.number_input("Empty_Index", value=0.0)
-Square_Index = st.number_input("Square_Index", value=0.0)
-Outside_X_Index = st.number_input("Outside_X_Index", value=1.0)
-Edges_X_Index = st.number_input("Edges_X_Index", value=0.0)
-Edges_Y_Index = st.number_input("Edges_Y_Index", value=0.0)
-Outside_Global_Index = st.number_input("Outside_Global_Index", value=1.0)
+Steel_Plate_Thickness = st.number_input("Steel_Plate_Thickness", value=80.0)
+Edges_Index = st.number_input("Edges_Index", value=0.0498)
+Empty_Index = st.number_input("Empty_Index", value=0.2415)
+Square_Index = st.number_input("Square_Index", value=0.1818)
+Outside_X_Index = st.number_input("Outside_X_Index", value=0.0047)
+Edges_X_Index = st.number_input("Edges_X_Index", value=0.4706)
+Edges_Y_Index = st.number_input("Edges_Y_Index", value=1)
+Outside_Global_Index = st.selectbox("Outside_Global_Index", options=[1, 0.5, 0])
+LogOfAreas = st.number_input("LogOfAreas" , value =2.4265)            
+Log_X_Index = st.number_input("Log_X_Index", value =0.9031)        
+Log_Y_Index = st.number_input("Log_Y_Index", value=1.6435)        
+Orientation_Index = st.number_input("Orientation_Index", value =0.8182)     
+Luminosity_Index = st.number_input("Luminosity_Index", value=-0.2913)  
+SigmoidOfAreas = st.number_input("SigmoidOfAreas", value = 0.5822)       
 
 # Manual one-hot encoding for TypeOfSteel
 A300 = 1 if TypeOfSteel == "A300" else 0
@@ -41,14 +47,16 @@ input_data = pd.DataFrame([[
     X_Perimeter, Y_Perimeter, Sum_of_Luminosity, Minimum_of_Luminosity,
     Maximum_of_Luminosity, Length_of_Conveyer, Steel_Plate_Thickness,
     Edges_Index, Empty_Index, Square_Index, Outside_X_Index,
-    Edges_X_Index, Edges_Y_Index, Outside_Global_Index,
-    A300, A400
+    Edges_X_Index, Edges_Y_Index, Outside_Global_Index,LogOfAreas,
+    Log_X_Index,Log_Y_Index,Orientation_Index,Luminosity_Index,SigmoidOfAreas
+    A300, A400,
 ]], columns=[
     'X_Minimum', 'X_Maximum', 'Y_Minimum', 'Y_Maximum', 'Pixels_Areas',
     'X_Perimeter', 'Y_Perimeter', 'Sum_of_Luminosity', 'Minimum_of_Luminosity',
     'Maximum_of_Luminosity', 'Length_of_Conveyer', 'Steel_Plate_Thickness',
     'Edges_Index', 'Empty_Index', 'Square_Index', 'Outside_X_Index',
-    'Edges_X_Index', 'Edges_Y_Index', 'Outside_Global_Index',
+    'Edges_X_Index', 'Edges_Y_Index', 'Outside_Global_Index', 'LogOfAreas',
+    'Log_X_Index', 'Log_Y_Index', 'Orientation_Index', 'Luminosity_Index', 'SigmoidOfAreas'
     'A300', 'A400'
 ])
 
