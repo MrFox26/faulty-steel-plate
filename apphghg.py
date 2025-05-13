db16aba35236c604fb7a86967743ae8450b2dee0
Jayant <jayantpr26@gmail.com>
2025-05-13 13:03:02 +0530

    Update apphghg.py

--- apphghg.py
+++ apphghg.py
@@ -79,4 +79,4 @@ target_mapping ={'Bumps': 0, 'Dirtiness': 1, 'K_Scatch': 2, 'Other_Faults': 3, '
 if st.button("Predict Fault Type"):
     prediction = model.predict(input_data)[0]
     predicted_label = target_mapping.get(prediction, "Unknown")
-    st.success(f"Predicted Fault Type: {predicted_label} (Label Encoded: {prediction})")
+    st.success(f"Predicted Fault Type: {predicted_label}")
